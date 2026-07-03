a65f21c17fc936400c5938e9e22e41109ab6aa74
Gaurav Rawat <gauravrawatcr@gmail.com>
2026-07-03 10:15:00 +0530

    Setup project skeleton, express configuration and package files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to TaskFlow API',
+    version: '1.0.0',
+    documentation: '/docs'
+  });
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
